d8229b6b4e11ac17f57aa392a887321be84fb732
Eduardo <eduardosiqueira0407@gmail.com>
2020-05-14 00:07:46 -0300

    Config prettier

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (_req, res) => res.json({ message: 'Acredite em seu potencial, vc é lgal' }));
+routes.get('/', (_req, res) =>
+  res.json({ message: 'Acredite em seu potencial, vc é lgal' })
+);
 
 export default routes;
